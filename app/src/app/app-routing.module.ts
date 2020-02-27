29f5bafaf0bff1e0a8fb4d86b91be776e171a78d
Himanshu Kumar <himanshu.kumar@cybergindia.local>
2020-02-27 12:23:03 +0530

    login component and admin crud list in angular

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { AdminCrudComponent } from './admin-crud/admin-crud.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login", component: LoginComponent, pathMatch:'full'},
+  {path:"admin", component:AdminCrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[AdminCrudComponent]
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login/login.component';
+import { AdminCrudComponent } from './admin-crud/admin-crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminCrudComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
