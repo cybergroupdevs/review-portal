28b0dbfaa476920f0ee8de190b82896e1ef75e81
nangchetnamaw <nangchetnamaw@gmail.com>
2020-02-27 13:22:27 +0530

    displaying details of particular user on ui

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AddUpdateUserComponent} from '../app/add-update-user/add-update-user.component'
+import{ServicesService} from './services.service';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +16,10 @@ import {AddUpdateUserComponent} from '../app/add-update-user/add-update-user.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
