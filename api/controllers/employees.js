76dc01f500e4a2aeca46c0fa0f3d1dfadad99c9d
Shubham Sharma <ss320163@gmail.com>
2020-02-27 11:47:30 +0530

    Wrote controller for login

--- api/controllers/employees.js
+++ api/controllers/employees.js
@@ -4,5 +4,32 @@ class Employee {
 
     }
 
+    jsonDecoder = (token) => {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+    };
+
+    async login(req, res) {
+        //Verify token here
+        if(/*token is valid*/true){
+            token = "knknkdksnainlasnndoidknkasnddoidknokndskdoisn"
+            decodedJson = this.jsonDecoder(token);
+            if(decodedJson.Designation == "Admin"){
+                //do some task according to admin
+                responseData = await model.employee.get({});
+                res.status(200).send("Successful");
+            }
+            else{
+                //do some task according to user
+            }
+        }
+        else{
+            res.status(401).send("Unauthorized");
+        }
+    }
 }
 module.exports = new Employee();
--- api/routes/route.js
+++ api/routes/route.js
@@ -1,12 +1,9 @@
 const controller = require('../controllers');
 
 module.exports = (app) => {
-
     //     const employee = new controller.employees()
     //     app.post("/test", (req,res) =>{
     //     console.log(req.body)
     //     res.send({session: `Testing HRMS`})
     // });
-
-    
 }
