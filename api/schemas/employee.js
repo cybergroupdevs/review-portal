654164580e4aa3d3e0335dc9d7e1a1aae9b4b8b7
akhtarfzn <38faizan@gmail.com>
2020-02-27 18:33:14 +0530

    updated homepage and admin dashboard UI

--- api/schemas/employee.js
+++ api/schemas/employee.js
@@ -1,52 +1,64 @@
 
 module.exports={
-firstName:{
-type:String,
-default:null
-},
-lastName:{
-    type:String,
-    default:null
+    cgiCode:{
+        type:String,
+        default:null
     },
-email:{
-type:String,
-required:true,
-unique:true
-},
-password:{
-    type:String
-},
-// phoneNo:{
-// type:String,
-// default:null
-// },
-location:{
-type:String,
-default:null
-},
-designation:{
-type:String,
-default:'Consultant 1',
-enum: ['Consultant 1','Consultant 2','Associate 1','Associate 2']
-},
-division: {
-    type:String
-},
-joined:
-{
-    type:Date,
-    default:Date.now()
+    firstName:{
+        type:String,
+        default:null
+    },
+    lastName:{
+        type:String,
+        default:null
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String
+    },
 
-},
-totalExperience:{
-    type:Number
-},
-previousExperience:{
-    type:Number
-},
-skills:[{
-    type:String
-}]
+    location:{
+        type:String,
+        default:null
+    },
+    designation:{
+        type:String,
+        default:'Consultant 1',
+        enum: ['Consultant 1','Consultant 2','Associate 1','Associate 2']
+    },
+    joined: {
+        type:Date,
+        default:Date.now()
+    },
+    totalExperience:{
+        type:Number,
+        default: 0
+    },
+    previousExperience:{
+        type:Number,
+        default: 0
+    },
+    skills:[{
+        type:String
+    }],
+    competenceManager: {
+        type: String
+    },
+    projectOwners: {
+        type: String
+    },
+    project: [{
+        type: String
+    }],
+    reviewer: {
+        type: String,
+    },
+    qualityAnalyst: {
+        type: String,
+    }
  
 }
-
