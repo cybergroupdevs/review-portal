16dcb097c217c0aa6c1a67e4211a42c8ad071299
Shivani Bansal <bansalshivani703@gmail.com>
2020-02-29 00:50:44 +0530

    added UI for review

---
+++ app/src/app/review-self-evaluation/review-self-evaluation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-self-evaluation',
+  templateUrl: './review-self-evaluation.component.html',
+  styleUrls: ['./review-self-evaluation.component.scss']
+})
+export class ReviewSelfEvaluationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
