f4da82cb42f985c02c59657cff7db69a366ca4b3
Shubham Sharma <ss320163@gmail.com>
2020-02-29 11:48:31 +0530

    Added routing for edit profile

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -21,5 +21,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
 export const routingComponents=[AdminCrudComponent]
