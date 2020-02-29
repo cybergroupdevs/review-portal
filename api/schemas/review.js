cfe559152b0e05b641e8961048aad8ab010b1e95
Shivani Bansal <bansalshivani703@gmail.com>
2020-02-29 19:33:07 +0530

    modified schemas added review schema

--- api/schemas/review.js
+++ api/schemas/review.js
@@ -1,25 +1,77 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
- 
-const reviewSchema = Schema({
+const Employee  = require('./employee').Schema;
 
-    employee: {
-        type: mongoose.Schema.Types.ObjectId,
+const reviewSchema = new mongoose.Schema({
+
+    employeeId: {
+        type: Schema.Types.ObjectId,
         ref: "Employee"
     },
     reviewer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review"
+        type: Schema.Types.ObjectId,
+        ref: "Employee"
     },
     qualityAnalyst: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review"
+        type: Schema.Types.ObjectId,
+        ref: "Employee"
     },
-    status: {
-        type: String
+    technicalSkill:{
+        comment:{
+            type:String,
+            default:null
+        },
+        binaryReview:{
+            type:String,
+            default:' Needs Improvement',
+            enum: ['Needs Improvement','Meets expectation']
+        }
     },
-    date: {
-        type: Date
+    personality:{
+        comment:{
+            type:String,
+            default:null
+        },
+        binaryReview:{
+            type:String,
+            default:' Needs Improvement',
+            enum: ['Needs Improvement','Meets expectation']
+        }
+    },
+    communication:{
+        content:{
+            type:String,
+            default:null
+        },
+        binaryReview:{
+            type:String,
+            default:' Needs Expectation',
+            enum: ['Needs Expectation','Meets expectation']
+        }
     },
 
+    date: {
+        type: Date,
+        default:Date.now()
+    },
+    
+    submitted:{
+        type:Boolean,
+        default:false
+    },
+    status:{
+        type:String,
+        default:"saved self appraisal",
+        enum:["saved self appraisal","Pending-Reviewer","Pending-QAer", "close"]
+    },
+    reviewCycle:{
+        type:String
+    },
+    promotionCycle:{
+        type:String
+    }
+
 });
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
