5a53e599a60424762fda92cd898ccd18b2ed504d
nangchetnamaw <nangchetnamaw@gmail.com>
2020-03-01 22:16:16 +0530

    updated reviewer typescript

--- app/src/app/services.service.ts
+++ app/src/app/services.service.ts
@@ -71,6 +71,14 @@ export class ServicesService {
     ));
   }
 
+  updateReviewData(userObj): Observable<any>{
+    //const params: any = new HttpParams().set('id', this.jsonDecoder(localStorage.getItem("JwtHrms")).data._id);
+    return this.http.get("http://localhost:3001/reviews/update/5e5b85cdb4f6bcd838db5e06").pipe(
+      tap(_ => this.log("updated review details")),
+      catchError(this.handleError<any>('error in updating details')
+    ));
+  }
+
   empData(): Observable<any>{
     return this.http.get(`http://localhost:3001/employee/show/${this.jsonDecoder(localStorage.getItem("JwtHrms")).data._id}`).pipe(
       tap(_ => this.log("received employee details")),
