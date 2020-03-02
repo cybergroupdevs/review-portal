58bb926d50ea24b465b57fb8a138cdad1e4f0a7c
horaanchal <horaanchal@gmail.com>
2020-03-02 19:33:25 +0530

    update UI improved

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -38,7 +38,7 @@ import { CreateReviewComponent } from './create-review/create-review.component';
     AddUserComponent,
     AdminOptionsComponent,
     ReviewerQaerComponent,
-    CreateReviewComponent
+    CreateReviewComponent,
     ReviewNavbarComponent,
     MyReviewsComponent,
     ReviewTableComponent,
