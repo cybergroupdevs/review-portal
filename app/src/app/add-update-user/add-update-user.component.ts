271293f6d6659397144b008d822409043c7f6b6f
akhtarfzn <38faizan@gmail.com>
2020-03-03 14:09:09 +0530

    modified update profile UI and added form validation

--- app/src/app/add-update-user/add-update-user.component.ts
+++ app/src/app/add-update-user/add-update-user.component.ts
@@ -7,7 +7,7 @@ import { ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
 @Component({
   selector: 'app-add-update-user',
   templateUrl: './add-update-user.component.html',
-  styleUrls: ['./add-update-user.component.css']
+  styleUrls: ['./add-update-user.component.scss']
 })
 export class AddUpdateUserComponent implements OnInit {
   @ViewChild('firstName', {static: false}) firstName: ElementRef;
