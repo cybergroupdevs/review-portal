4f10aefa1810876d40096079576517504c0993e3
nangchetnamaw <nangchetnamaw@gmail.com>
2020-03-03 13:09:34 +0530

    missing files added

--- app/src/app/reviewer-review/reviewer-review.component.ts
+++ app/src/app/reviewer-review/reviewer-review.component.ts
@@ -143,6 +143,7 @@ export class ReviewerReviewComponent implements OnInit {
           console.log('unsuccessful');
          
         }
+        
       
       });
       this.updateReviewDetails();
