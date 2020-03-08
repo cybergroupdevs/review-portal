727f2aa9bce8af25b3bdf5e0920ca9fbf90de471
horaanchal <horaanchal@gmail.com>
2020-03-08 12:07:52 +0530

    possible validations added

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -48,9 +48,6 @@ export class LoginComponent implements OnInit {
       else if(res.status == 401){
         alert("Unauthorized");
       }
-    //   else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-    //   { return (true) } alert("You have entered an invalid email address!") 
-    //   return (false) 
     });    
   }
 
