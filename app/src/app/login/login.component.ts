46c52548d85e751fec75993583138f14a32a67cf
nangchetnamaw <nangchetnamaw@gmail.com>
2020-03-10 22:16:22 +0530

    client side validation on empty fields

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -18,6 +18,20 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+        
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('form-horizontal myForm');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+        });
+
   }
   
   checkUser(){
